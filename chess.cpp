6e0a1051d6836418148a4f87611f4ea35fbfd1bb
HHayk004 <hayk.hovsepyan004@gmail.com>
2024-02-16 16:24:17 +0400

    Adding refresh terminal and check idea:

--- chess.cpp
+++ chess.cpp
@@ -1,12 +1,15 @@
 #include "chess.h"
+#include <cstdlib>
 
 int main()
 {
+    std::system("clear");
     Chess game;
     while (true)
     {
         game.printBoard();
         game.makeMove();
+        std::system("clear");
     }
 
     return 0;
